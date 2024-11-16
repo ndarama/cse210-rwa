c01f114e50c9f4302c03e5b745968a24edc53a87
Ndarama <ndaramartwork@gmail.com>
2024-11-16 13:44:25 +0200

    Journel Program v.1.1

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -13,8 +13,8 @@ public class Program
             Console.WriteLine("Journal Program Menu:");
             Console.WriteLine("1. Write a new entry");
             Console.WriteLine("2. Display all entries");
-            Console.WriteLine("3. Save journal to file");
-            Console.WriteLine("4. Load journal from file");
+            Console.WriteLine("3. Save journal");
+            Console.WriteLine("4. Load journal");
             Console.WriteLine("5. Exit");
             Console.Write("Choose an option: ");
             string choice = Console.ReadLine();
